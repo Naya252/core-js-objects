5290679641849d3dbd2c5f4259af9021ab73357e
Naya252 <naya252naya@gmail.com>
2024-03-02 11:56:14 +0300

    feat: add solution to sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -171,8 +171,20 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let sum = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] > 25 && queue[i] > sum) {
+      return false;
+    }
+    if (queue[i] > 25 && queue[i] > sum) {
+      sum -= queue[i];
+    }
+    if (queue[i] === 25) {
+      sum += 25;
+    }
+  }
+  return true;
 }
 
 /**
